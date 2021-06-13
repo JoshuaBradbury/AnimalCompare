fd5da69d85689f09f2b3a82e836acc46230e0a03
JoshuaBradbury <joshua.bradbury@protonmail.com>
2021-06-13 18:30:58 +0100

    Reintroduce animals based on certain thresholds

--- internal/data/src/main/kotlin/uk/co/newagedev/animalcompare/data/room/daos/AnimalDao.kt
+++ internal/data/src/main/kotlin/uk/co/newagedev/animalcompare/data/room/daos/AnimalDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import uk.co.newagedev.animalcompare.domain.model.Animal
+import uk.co.newagedev.animalcompare.domain.model.AnimalType
 
 @Dao
 interface AnimalDao {
@@ -13,4 +14,10 @@ interface AnimalDao {
 
     @Query("SELECT EXISTS(SELECT * FROM animals WHERE url = :url)")
     suspend fun doesExist(url: String): Boolean
+
+    @Query("SELECT ID FROM animals WHERE type = :animalType ORDER BY RANDOM() LIMIT :limit")
+    suspend fun getRandomAnimals(animalType: AnimalType, limit: Int): List<Int>
+
+    @Query("SELECT COUNT(*) FROM animals WHERE type = :animalType")
+    suspend fun getAnimalCountByType(animalType: AnimalType): Int
 }
